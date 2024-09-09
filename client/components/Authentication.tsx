c3edce321aef8a14b5eee134447824062cb13d54
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2024-09-09 14:11:57 +1200

    add Authentication.tsx

---
+++ client/components/Authentication.tsx
@@ -0,0 +1,22 @@
+// TODO: import useAuth0 function
+
+import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
+
+const useIsAuthenticated = () => {
+  const { isAuthenticated } = useAuth0()
+  console.log(isAuthenticated)
+  return isAuthenticated
+}
+interface Props {
+  children: React.ReactNode
+}
+export function IfAuthenticated(props: Props) {
+  const { children } = props
+  return useIsAuthenticated() ? <>{children}</> : null
+}
+
+export function IfNotAuthenticated(props: Props) {
+  const { children } = props
+  return !useIsAuthenticated() ? <>{children}</> : null
+}
