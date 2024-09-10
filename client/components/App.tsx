13614b7185b38f7c92ed1de11c11b6c831c721cf
Fatima Yarhamidi <fatima.yarhamidi@gmail.com>
2024-09-10 17:02:37 +1200

    add header

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -5,36 +5,42 @@ import SearchBar from './UI/search bar/SearchBar.tsx'
 import { User, UserRepos } from '../../models/user.ts'
 
 function App() {
-  const [username, setUsername] = useState<string>('')  // Start with an empty string
+  // const [username, setUsername] = useState<string>('') // Start with an empty string
 
-  // Destructure data, error, and isLoading from the result of useUser
-  const { data: userData, error: userError, isLoading: userLoading } = useUser(username)
+  // // Destructure data, error, and isLoading from the result of useUser
+  // const {
+  //   data: userData,
+  //   error: userError,
+  //   isLoading: userLoading,
+  // } = useUser(username)
 
-  
-  const { data: repoData, error: repoError, isLoading: repoLoading } = useUserRepos(username)
+  // const {
+  //   data: repoData,
+  //   error: repoError,
+  //   isLoading: repoLoading,
+  // } = useUserRepos(username)
 
-  const [currentUserData, setCurrentUserData] = useState<User | null>(null)
-  const [currentRepoData, setCurrentRepoData] = useState<UserRepos[] | null>(null)
+  // const [currentUserData, setCurrentUserData] = useState<User | null>(null)
+  // const [currentRepoData, setCurrentRepoData] = useState<UserRepos[] | null>(
+  //   null,
+  // )
 
-  // Reset data when username changes and refetch data
-  useEffect(() => {
-    
-    
-    setCurrentUserData(null)
-    setCurrentRepoData(null)
+  // // Reset data when username changes and refetch data
+  // useEffect(() => {
+  //   setCurrentUserData(null)
+  //   setCurrentRepoData(null)
 
-    if (userData) {
-      setCurrentUserData(userData)
-    }
-    if (repoData) {
-      setCurrentRepoData(repoData)
-    }
-  }, [userData, repoData, username])
+  //   if (userData) {
+  //     setCurrentUserData(userData)
+  //   }
+  //   if (repoData) {
+  //     setCurrentRepoData(repoData)
+  //   }
+  // }, [userData, repoData, username])
 
-
-
-  if (userLoading || repoLoading) return <div>Loading...</div>
-  if (userError || repoError) return <div>Error: {userError?.message || repoError?.message}</div>
+  // if (userLoading || repoLoading) return <div>Loading...</div>
+  // if (userError || repoError)
+  //   return <div>Error: {userError?.message || repoError?.message}</div>
 
   return (
     <>
@@ -43,7 +49,7 @@ function App() {
         <h1 className="text-3xl font-bold  underline">
           Fullstack Boilerplate - with Fruits!
         </h1>
-        <SearchBar setUsername={setUsername} />
+        {/* <SearchBar setUsername={setUsername} />
         {currentUserData ? (
           <>
             <p>{currentUserData.name}</p>
@@ -58,7 +64,7 @@ function App() {
           ) : (
             <p>No repositories found</p>
           )}
-        </ul>
+        </ul> */}
       </div>
     </>
   )
